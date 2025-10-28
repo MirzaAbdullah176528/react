d1fe1fa799fc9e771d78603df58b18b54a7729c9
Abdullah <ajjh564356165649@gmail.com>
2025-10-28 17:32:56 +0500

    ok

--- app/tictactoe/page.tsx
+++ app/tictactoe/page.tsx
@@ -1,13 +1,27 @@
 "use client";
+import { updateTag } from "next/cache";
 import { useState } from "react";
+import { useEffect } from "react";
 
 
+function O_shapes(){
+  return(
+    <div className="O_shape">
+    <p className="inside_circle"></p>
+    </div>
+  )
+}
+function X_shape(){
+  return(
+    <div className="x-shape"></div>
+  )
+}
 function Square({value,onSquareClick}:{value: string | null | 0; onSquareClick: () => void;}){
   return (
     <button
       className="square"
       onClick={onSquareClick}
-      style={{ width: "150px", height: "100px", fontSize: "32px" }}
+      style={{ width: "100px", height: "100px", fontSize: "32px" , margin: '0.3rem'}}
     >
       {value}
     </button>
@@ -17,13 +31,30 @@ function Square({value,onSquareClick}:{value: string | null | 0; onSquareClick: 
 export default function Board() {
   const [xisnext , setXisNext] =useState(true)
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [scores , setScores] = useState([0, 0])
+
+  
   const winner = calculateWinner(squares)
+  
+
+  function Set_winner(winner:any){
+    setScores(prev => {
+      const updated = [...prev]
+      if(winner == 'X'){
+        updated[0] = updated[0] + 1  
+      } else if(winner == 'O'){
+        updated[1] = updated[1] + 1
+      }
+      return updated
+    })
+  }
+
   let status;
   if(winner){
-    status = 'Winner'+winner;
+    status = `Winner : ${winner}`;
   }
   else{
-    status = 'Next player'+(xisnext? 'X':'O')
+    status = `Next player : ${xisnext? 'X':'O'}`
   }
 
   function handleClick(i: number) {
@@ -40,8 +71,84 @@ export default function Board() {
     setSquares(nextSquares);
     setXisNext(!xisnext)
   }
+  function if_game_end(){
+    return;
+  }
 
-  return (
+  function re_match(){
+    return setSquares(Array(9).fill(null)) 
+  }
+const btn = <button onClick={re_match}>Start New game</button>
+
+function Reset_score(){
+  setScores([0,0])
+  re_match()
+}
+const btn_reset = <button onClick={Reset_score} >Resart</button>
+
+
+  function New_game(){
+  return(
+    <div className="score">
+      <div className="score_card">
+    <div className="X_o">
+        <h1 style={{fontSize:'1.7rem'}}>Player 1</h1>
+        {X_shape()}
+        <p>Score = {scores[0]}</p>
+      </div>
+    <div className="X_o">
+        <h1 style={{fontSize:'1.7rem'}}>Player 2</h1>
+        {O_shapes()}
+        <p>Score = {scores[1]}</p>
+      </div>
+    </div>
+    <div className="score_btn">
+      {btn}
+      {btn_reset}
+      </div>
+    </div>
+    
+  )
+}
+
+useEffect(() => {
+  if (winner) {
+    Set_winner(winner);
+  }
+}, [winner]);
+
+
+  
+  if(status.includes(winner)){
+    return (
+      <div className="main_with_score">
+    <div className="main">
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => if_game_end} />
+        <Square value={squares[1]} onSquareClick={() => if_game_end} />
+        <Square value={squares[2]} onSquareClick={() => if_game_end} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => if_game_end} />
+        <Square value={squares[4]} onSquareClick={() => if_game_end} />
+        <Square value={squares[5]} onSquareClick={() => if_game_end} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => if_game_end} />
+        <Square value={squares[7]} onSquareClick={() => if_game_end} />
+        <Square value={squares[8]} onSquareClick={() => if_game_end} />
+      </div>
+      <div className="square" >{status}</div>
+    </div>
+    <div className="new_start">
+      < New_game />
+    </div>
+    </div>
+  );
+  }
+  else{
+return (
+      <div className="main_with_score">
     <div className="main">
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
@@ -60,9 +167,13 @@ export default function Board() {
       </div>
       <div className="square" >{status}</div>
     </div>
+    <div className="new_start">
+      < New_game />
+    </div>
+    </div>
+    
   );
 }
-
 
 function calculateWinner(squares:any[]) {
   const lines = [
@@ -83,3 +194,6 @@ function calculateWinner(squares:any[]) {
   }
   return null;
 }
+}
+
+
